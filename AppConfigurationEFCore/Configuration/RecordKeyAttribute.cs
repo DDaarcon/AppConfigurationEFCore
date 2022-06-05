fbb0d02cf087868b3d16415641813f3273323808
Dariusz Wiktorowski <dariusz.wiktorowski@investit.dev>
2022-06-05 04:06:14 +0200

    Add project files.

---
+++ AppConfigurationEFCore/Configuration/RecordKeyAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace AppConfigurationEFCore.Configuration
+{
+    /// <summary>
+    /// Attribute, that has to be present at each property of <c>TRecords</c> of <see cref="IAppConfiguration{TRecords}"/>.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class RecordKeyAttribute : Attribute
+    {
+        public string Key { get; }
+        public RecordKeyAttribute(string key)
+        {
+            Key = key;
+        }
+    }
+}
