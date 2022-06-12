ac2e10c7e7e971d20fca640ea39858cb739d860f
DDaarcon <wiktorowski.dariusz1@gmail.com>
2022-06-12 18:09:33 +0200

    Missing tests

--- Tests/NestedConfigurationTests.cs
+++ Tests/NestedConfigurationTests.cs
@@ -4,9 +4,7 @@ using AppConfigurationEFCore.Entities;
 using AppConfigurationEFCore.Setup;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Tests.Help.Db;
 
@@ -159,6 +157,47 @@ namespace Tests
             public VTRecordHandler<int> Bbb { get; set; } = null!;
         }
 
+        [Test]
+        public async Task ShouldCreateDoubleNestedGroup()
+        {
+            TestServices.Collection.AddAppConfiguration<MyDbContext, ValidConfigRecords5>();
+            var svc = TestServices.GetService<IAppConfiguration<ValidConfigRecords5>>();
+
+            await svc.Records.Inner.Bbb.Aaa.SetAndSaveAsync("aaaVal");
+
+            TestDatabase.Context.Set<AppConfig>().Any(x => x.Key == "inner..inner.aaa");
+
+            var saved = await svc.Records.Inner.Bbb.Aaa.GetAsync();
+
+            Assert.IsNotNull(saved);
+            Assert.AreEqual("aaaVal", saved);
+        }
+        private class ValidConfigRecords5
+        {
+            [RecordKey("aaa")]
+            public RecordHandler<string> Aaa { get; set; } = null!;
+            [RecordKey("bbb")]
+            public VTRecordHandler<int> Bbb { get; set; } = null!;
+            [RecordGroup(GroupKey = "inner", Separator = "..")]
+            public ValidSubConfigRecords5 Inner { get; private set; } = null!;
+        }
+        private class ValidSubConfigRecords5
+        {
+            [RecordKey("aaa")]
+            public RecordHandler<string> Aaa { get; set; } = null!;
+
+            [RecordGroup(GroupKey = "inner")]
+            public ValidNestedSubConfigRecords5 Bbb { get; set; } = null!;
+        }
+        private class ValidNestedSubConfigRecords5
+        {
+            [RecordKey("aaa")]
+            public RecordHandler<string> Aaa { get; set; } = null!;
+
+            [RecordKey("bbb")]
+            public VTRecordHandler<int> Bbb { get; set; } = null!;
+        }
+
 
 
         #region Fails
@@ -280,6 +319,72 @@ namespace Tests
             public RecordHandler<string> Aaa { get; set; } = null!;
 
             public VTRecordHandler<int> Bbb { get; set; } = null!;
+        }
+
+        [Test]
+        public void ShouldFail_WhenRecursiveGroup()
+        {
+            try
+            {
+                TestServices.Collection.AddAppConfiguration<MyDbContext, InvalidConfigRecords5>();
+                TestServices.GetService<IAppConfiguration<InvalidConfigRecords5>>();
+                Assert.IsTrue(false);
+            }
+            catch (Exception)
+            { }
+        }
+        private class InvalidConfigRecords5
+        {
+            [RecordKey("aaa")]
+            public RecordHandler<string> Aaa { get; set; } = null!;
+            [RecordKey("bbb")]
+            public VTRecordHandler<int> Bbb { get; set; } = null!;
+            public InvalidSubConfigRecords5 Inner { get; private set; } = null!;
+        }
+        [RecordGroup(GroupKey = "inner")]
+        private class InvalidSubConfigRecords5
+        {
+            [RecordKey("aaa")]
+            public RecordHandler<string> Aaa { get; set; } = null!;
+
+            public InvalidSubConfigRecords5 Recursive { get; set; } = null!;
+        }
+
+        [Test]
+        public void ShouldFail_WhenRecursiveGroupSequence()
+        {
+            try
+            {
+                TestServices.Collection.AddAppConfiguration<MyDbContext, InvalidConfigRecords6>();
+                TestServices.GetService<IAppConfiguration<InvalidConfigRecords6>>();
+                Assert.IsTrue(false);
+            }
+            catch (Exception)
+            { }
+        }
+        private class InvalidConfigRecords6
+        {
+            [RecordKey("aaa")]
+            public RecordHandler<string> Aaa { get; set; } = null!;
+            [RecordKey("bbb")]
+            public VTRecordHandler<int> Bbb { get; set; } = null!;
+            public InvalidSubConfigRecords6 Inner { get; private set; } = null!;
+        }
+        [RecordGroup(GroupKey = "inner")]
+        private class InvalidSubConfigRecords6
+        {
+            [RecordKey("aaa")]
+            public RecordHandler<string> Aaa { get; set; } = null!;
+
+            public InvalidNestedSubConfigRecords6 Recursive { get; set; } = null!;
+        }
+        [RecordGroup(GroupKey = "inner")]
+        private class InvalidNestedSubConfigRecords6
+        {
+            [RecordKey("aaa")]
+            public RecordHandler<string> Aaa { get; set; } = null!;
+
+            public InvalidSubConfigRecords5 Recursive { get; set; } = null!;
         }
 
 
